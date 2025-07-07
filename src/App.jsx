448ab1c93f1ce707ad1209435dfb87c6a0c26807
Nhi Nguyen <lamnhi.ngngoc@gmail.com>
2025-07-07 06:23:52 +0300

    Merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/header";
 import Main from "./pages/main/main";
 import CoursePage from "./pages/CoursePage/CoursePage";
-import Vocabulary from "./module1/Vocabulary"; //
+import Vocabulary from "./module1/Vocabulary"; 
+import Module3 from "./pages/module3/module3";
 
 function App() {
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<><Main /></>} />
+        <Route path="/" element={<><Main /><Module3 /></>} />
         <Route path="/course" element={<CoursePage />} />
         <Route path="/course/a1/lesson-1/vocabulary" element={<Vocabulary />} />
       </Routes>
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,7 @@
-// src/utils/api.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api", // 
+  baseURL: "http://localhost:3000/api", 
   timeout: 10000,
 });
 
