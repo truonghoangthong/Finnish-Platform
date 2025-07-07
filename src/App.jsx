262efd640c41f3e38c83f2f43506ed0b67d211ab
unknown <nltrangthanh181293@gmail.com>
2025-07-07 10:05:34 +0300

    edit Courspage button funtions, added navigation, edit path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/header/header";
 import Main from "./pages/main/main";
 import CoursePage from "./pages/CoursePage/CoursePage";
-import Vocabulary from "./pages/module1/Vocabulary"; 
+import Vocabulary from "./pages/module1/Vocabulary"; // ✅ Phải viết hoa 'V'
+
 import Module3 from "./pages/module3/module3";
 
 function App() {
@@ -12,7 +13,11 @@ function App() {
       <Routes>
         <Route path="/" element={<><Main /><Module3 /></>} />
         <Route path="/course" element={<CoursePage />} />
-        <Route path="/course/a1/lesson-1/vocabulary" element={<Vocabulary />} />
+    
+        <Route
+          path="/course/a1/lesson-1/vocabulary"
+          element={<Vocabulary />}
+        />
       </Routes>
     </Router>
   );
