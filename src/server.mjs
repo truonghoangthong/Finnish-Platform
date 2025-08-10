51aba0d6cfe76bbc7a6a14152321772e13d36ed2
truonghoangthong <126312478+truonghoangthong@users.noreply.github.com>
2025-08-11 00:16:38 +0300

    update index.js

--- src/server.mjs
+++ src/server.mjs
@@ -275,10 +275,6 @@ app.post('/api/new_lesson', async (req, res) => {
   }
 });
 
-export const createServer = () => app;
-export default app;
-
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
