4da74ddef887668911f36d98788757a7d61506b0
truonghoangthong <126312478+truonghoangthong@users.noreply.github.com>
2025-08-18 14:16:00 +0300

    change API domains

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://finnish-platform-thong-truongs-projects.vercel.app/api",
   timeout: 10000,
 });
 
